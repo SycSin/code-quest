f571beea65e6b581e51235397ae9ffe6d28db6a9
Curt96 <92109778+Curt96@users.noreply.github.com>
2024-04-05 15:54:36 +0200

    Added ingame Menu :)

--- UnityWorkshop/Assets/Main_Menu.cs
+++ UnityWorkshop/Assets/Main_Menu.cs
@@ -16,3 +16,4 @@ public class Main_Menu : MonoBehaviour
             Application.Quit();
         }
 }
+//player settings 
---
+++ UnityWorkshop/Assets/Pause_Menu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Menu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    
+    public GameObject pauseMenuUI;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        //resume the game
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+  
+    void Pause()
+    {
+        //pause the game
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+    
+    
+}
